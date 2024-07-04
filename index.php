0e7ad183a9f87d5861041461788f108324f1110b
ibroid <maulana858@gmail.com>
2024-07-04 15:11:17 +0700

    first init

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+// function task_one(string $input = "aaabbcccddeddbzaa")
+// {
+//   // convert to array
+
+//   // $container = [];
+
+//   // print_r($input);
+// }
+
+function task_two(array $par =  [9, 3, 7, 8, 2, 6, 1, 4, 10, 2, 2, 3])
+{
+  $container = [];
+  $temp = 0;
+  $selected = [
+    'index' => 0,
+    'value' => 0
+  ];
+
+  foreach ($par as $n => $v) {
+    if ($n == 0) {
+      $temp = $v;
+      $container[$n] = $v;
+      return;
+    }
+
+    if ($n > 0) {
+      foreach ($container as $y => $x) {
+        if ($v > $x) {
+          $selected['index'] = $y;
+          $selected['value'] = $v;
+        }
+
+        $explited = array_slice($container, $selected['index'] - 1);
+
+        $container[$selected['index']] = $selected['value'];
+
+        $container = array_merge($container, $explited);
+      }
+    }
+  }
+
+  var_export($container);
+}
+
+// task_one();
+task_two();
---
+++ readme.md
@@ -0,0 +1,16 @@
+1. Buatlah fungsi yang dapat menghasilkan output a5b3c3d4ez dari
+   inputan aaabbcccddeddbzaa
+2. Urutkan array dibawah ini dari yang terkecil sampai terbesar dengan
+   menggunakan perulangan dan kondisi (jangan menggunakan
+   sorting function dari bahasa pemrograman)
+   Array = [9,3,7,8,2,6,1,4,10,2,2,3]
+3. Harga untuk tipe barang A adalah Rp. 99.900,-. Jika membeli dalam
+   jumlah lebih dari 50 maka akan mendapatkan diskon 5% dan jika
+   membeli di hari Senin atau Kamis maka akan mendapatkan diskon
+   tambahan sebesar 10%.
+   Harga untuk tipe barang B adalah Rp. 49.900,-. Jika membeli barang
+   dalam jumlah lebih dari 100 maka akan mendapatkan diskon 10%
+   dan jika membeli di hari Jumat maka akan mendapat diskon
+   tambahan 5%.
+   Input = tipe barang, jumlah barang
+   Output = harga akhir
